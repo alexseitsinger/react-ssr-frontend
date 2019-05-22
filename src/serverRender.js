2ddb0e4b57bf866e31024d9b027f81c48717bc64
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-22 23:47:06 +0000

    refactor: changes server function

---
+++ src/serverRender.js
@@ -0,0 +1,35 @@
+import { createMemoryHistory } from "history"
+
+/**
+ * @name  serverRender
+ * @description  The entry point for the server-side bundle.
+ * @param  {Function} configureStore Creates the store object.
+ * @param  {Function} render Creates the rendered app output.
+ * @return {Function} Takes arugments (request, errback). When invoked, will either run the render or the errback.
+ */
+export default (configureStore, render) => (request, errback) => {
+	try {
+		const { url, initialState } = request.body
+
+		// Create a history entry based on the URL of the requested page.
+		const history = createMemoryHistory({
+			initialEntries: [url],
+			initialIndex: 0
+		})
+
+		// Create the store to use for the render.
+		const store = configureStore(history, initialState)
+
+		// Render the component.
+		render(request, store, history, callback)
+	} catch (e) {
+		// Return an error object to django.
+		errback({
+			error: {
+				type: e.constructor.name,
+				message: e.message,
+				stack: e.stack
+			}
+		})
+	}
+}
