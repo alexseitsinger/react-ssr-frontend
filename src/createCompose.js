f5918f823c22bd1642c772a4975e485c2d2453de
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-22 23:08:16 +0000

    refactor: removes provider and condenses output

--- src/createCompose.js
+++ src/createCompose.js
@@ -1,16 +1,7 @@
 import React from "react"
-import { Provider } from "react-redux"
-import { ConnectedRouter } from "connected-react-router"
 
-function createCompose(app) {
-	function compose(store, history) {
-		return (
-			<Provider store={store}>
-				<ConnectedRouter history={history}>{app}</ConnectedRouter>
-			</Provider>
-		)
-	}
-	return compose
+function createCompose(App) {
+	return (store, history) => <App store={store} history={history} />
 }
 
 export default createCompose
