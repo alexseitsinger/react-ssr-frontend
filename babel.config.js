4e8d98e9ab483a422f0d6391853e9805e763ea12
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-22 22:58:59 +0000

    chore: changes to .babelrc

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	presets: ["@babel/preset-env", "@babel/preset-react"],
-	plugins: [
-		"@babel/plugin-proposal-export-default-from",
-		"@babel/plugin-proposal-export-namespace-from",
-		"@babel/plugin-syntax-export-default-from",
-		"@babel/plugin-syntax-export-namespace-from"
-	]
-}
