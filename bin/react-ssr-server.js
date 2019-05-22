1819ea6cafadd7af7961bc99a29f0f0cc29c141f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-22 23:58:21 +0000

    refactor: renamed some variables, changed formatting

--- bin/react-ssr-server.js
+++ bin/react-ssr-server.js
@@ -52,7 +52,7 @@ yargs
 		default: "secret-key"
 	})
 	.option("bundle", {
-		description: "The bundle that contains our server bundle."
+		description: "The bundle that contains our server-side app."
 	})
 	.help("h")
 	.alias("h", "help")
@@ -75,44 +75,58 @@ const {
 } = yargs.argv
 
 function getBundle() {
-	var render
+    // The variable to store our server-side render in.
+    var serverRender
+    // The paths possible to search from.
 	const bundlePaths = [
 		bundle,
 		path.resolve(__dirname, `./${bundle}`),
 		path.resolve(__dirname, `../../${bundle}`),
 		path.resolve(__dirname, `../../../../${bundle}`),
-	]
+    ]
+    // Keep a list of each attempt that fails or succeeds.
 	const failed = []
-	const succeeded = []
-	while (bundlePaths.length){
-		const bundlePath = bundlePaths.shift()
-		if(!render){
-			const result = {"path": bundlePath}
-			try {
-				render = require(bundlePath).default
+    const succeeded = []
+    // While the bundle paths are remaining, run this function.
+    while (bundlePaths.length){
+        // Get the item from the top of the list.
+        const bundlePath = bundlePaths.shift()
+        // If the serverRender variable is empty, continue...
+        if(!serverRender){
+            // Create a result object to make our report from.
+            const result = {"path": bundlePath}
+            // Try to import the bundle using the patch provided.
+            try {
+                // If the import is successful, add it to the success list.
+				serverRender = require(bundlePath).default
 				succeeded.push(result)
 			}
-			catch (e) {
+            catch (e) {
+                // If the import fails, create an error message print after.
 				result["error"] = {
 					"type": e.name,
 					"message": e.message,
-				}
+                }
+                // And add it to the list of failed bundle paths.
 				failed.push(result)
 			}
 		}
-	}
-	if(!render){
+    }
+    // If the serverRender path is empty, print a message for each path tried.
+	if(!serverRender){
 		failed.forEach((obj) => {
 			console.log(`Import failed: ${obj.path}`)
 			console.log(`    Reason: ${obj.error.type} - ${obj.error.message}`)
 		})
-	}
+    }
+    // Otherwise, print a message for the imports that succeeded.
 	else {
 		succeeded.forEach((obj) => {
 			console.log(`Import succeeded: ${obj.path}`)
 		})
-	}
-	return render
+    }
+    // Return the serverRender object we found.
+	return serverRender
 }
 
 function hasSecretKey(req) {
@@ -150,15 +164,11 @@ app.get(`${statsUrl}/:agentName/:environmentName`, (req, res) => {
 	const statsFile = path.resolve(
 		`./${statsPath}/${statsFileName}.${agentName}.${environmentName}.json`
 	)
-	readFile(
-		statsFile,
-		(data) => {
-			res.json(JSON.parse(data))
-		},
-		() => {
-			res.status(404).end()
-		}
-	)
+	readFile(statsFile, (data) => {
+		res.json(JSON.parse(data))
+	}, () => {
+		res.status(404).end()
+	})
 })
 
 // Returns the json data for the default state of a reducer.
@@ -168,29 +178,25 @@ app.get(`${stateUrl}/:reducerName`, (req, res) => {
 	}
 	const { reducerName } = req.params
 	const stateFile = path.resolve(
-		`./${statePath}/${reducerName}/${stateFileName}`
+        `./${statePath}/${reducerName}/${stateFileName}`
 	)
-	readFile(
-		stateFile,
-		(data) => {
-			res.json(JSON.parse(data))
-		},
-		() => {
-			res.status(404).end()
-		}
-	)
+	readFile(stateFile, (data) => {
+		res.json(JSON.parse(data))
+	}, () => {
+		res.status(404).end()
+	})
 })
 
 // Returns the rendered react component data.
 app.post(renderUrl, (req, res) => {
-	const render = getBundle()
-	if (!render) {
+	const serverRender = getBundle()
+	if (!serverRender) {
 		return res.status(500).end()
 	}
 	if (!hasSecretKey(req)) {
 		return res.status(400).end()
 	}
-	render(req, (result) => {
+	serverRender(req, (result) => {
 		res.json(result)
 	})
 })
