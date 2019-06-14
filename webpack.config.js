b3ff8f3f217d452272c0e2f133b955bb92d41095
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-14 02:37:16 -0400

    chore: Adds nodeExternals.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require("path")
+const nodeExternals = require("webpack-node-externals")
 
 module.exports = {
-	entry: "./src/index.js",
-	mode: "production",
-	target: "node",
-	output: {
-		path: path.resolve("./dist"),
-		filename: "[name].js",
-		libraryTarget: "commonjs2"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				use: "babel-loader",
-				exclude: /node_modules/
-			}
-		]
-	}
+  entry: "./src/index.js",
+  mode: "production",
+  target: "node",
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: [
+    nodeExternals({
+      modulesFromFile: {
+        exclude: ["dependencies"],
+        include: ["peerDependencies", "devDependencies", "optionalDependencies"],
+      },
+    }),
+  ],
 }
