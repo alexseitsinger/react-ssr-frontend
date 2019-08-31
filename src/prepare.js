5ab0b67041b7a6d00f52d779599f4a573a575788
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-31 16:09:38 -0400

    refactor: Renamed composer to prepare.

---
+++ src/prepare.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+/**
+ * Wraps the App component so it uses specific props.
+ *
+ * @param {object} App
+ * The component to wrap.
+ *
+ * @return {function}
+ * A component using the required props.
+ *
+ * @example
+ * import { prepared } from "@alexseitsinger/react-ssr"
+ *
+ * import { App } from "./app"
+ *
+ * export default prepared(App)
+ */
+export const prepare = App => ({ store, history, ...rest }) => (
+  <App store={store} history={history} {...rest} />
+)
