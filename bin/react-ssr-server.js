3c54f007e3595f03922955146c7428ba39d2ea09
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-11-21 22:52:54 -0500

    feat: Moves bulk of script to utils module.

--- bin/react-ssr-server.js
+++ bin/react-ssr-server.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs")
+const fs = require("fs")
 const path = require("path")
+
+const yargs = require("yargs")
 const express = require("express")
 const bodyParser = require("body-parser")
-const fs = require("fs")
+
+const {
+  configure,
+  setNoCacheHeaders,
+  getFirstExistingFile,
+  logMessage,
+} = require("../utils")
 
 // Capture the options
 yargs
@@ -77,14 +85,6 @@ yargs
     default: "server.js",
     string: true,
   })
-  .option("before", {
-    describe: "The method to run before listening.",
-    string: true,
-  })
-  .option("after", {
-    describe: "The method to run after listening.",
-    string: true,
-  })
   .option("allowedFiles", {
     describe: "Files that are allowed to be read.",
     default: [],
@@ -120,311 +120,59 @@ const {
   secretKeyHeaderName,
   bundlePath,
   bundleName,
-  before,
-  after,
   allowedFiles,
   allowedFiletypes,
   ignoredFiles,
 } = yargs.argv
 
-// The project root
 const root = path.resolve(".")
 
-// The paths used to find the bundle on the filesystem.
-const bundlePathSuffix = path.join(bundlePath, bundleName)
-const bundlePaths = [
+const {
+  options: {
+    paths,
+    allowed,
+    allowedTypes,
+    ignored,
+  },
+  methods: {
+    readResponse,
+    renderResponse,
+  },
+} = configure(
+  root,
+  bundlePath,
   bundleName,
-  path.resolve(__dirname, `./${bundlePathSuffix}`),
-  path.resolve(__dirname, `../../${bundlePathSuffix}`),
-  path.resolve(__dirname, `../../../../${bundlePathSuffix}`),
-].filter(bp => {
-  // Remove paths to bundles that exist outside the scope of the root.
-  if (isOutsideRoot(bp)) {
-    return false
-  }
-  return true
-})
-
-// The only files that are allowed to be read.
-const allAllowedFiles = [statsFileName, stateFileName].concat(allowedFiles)
-
-// The only filetypes that are allowed to be read.
-const allAllowedFiletypes = [".json"].concat(allowedFiletypes)
-
-// The only files that are never allowed to be read.
-const allIgnoredFiles = [".env"].concat(ignoredFiles)
-
-function logMessage(lines) {
-  const first = `[react-ssr]: ${lines.shift()}`
-  const rest = lines.map(line => `  ${line}`)
-  const final = ([first].concat(rest)).join("\n")
-  console.log(final)
-}
-
-function isFunction(f) {
-  return Boolean(typeof f === "function")
-}
-
-function importDefault(pathToModule) {
-  var b
-  try {
-    b = require(pathToModule).default
-  }
-  catch (e) {
-    b = require(pathToModule)
-  }
-  return b
-}
-
-// Returns true if the header exists and it matches the key specified.
-function hasSecretKey(req) {
-  const header = req.get(secretKeyHeaderName)
-  if (!secretKey || header === secretKey) {
-    return true
-  }
-}
-
-// Prevent reading of certain files always.
-function isIgnoredFile(target) {
-  const targetPath = path.resolve(target)
-  const targetFile = path.basename(targetPath)
-  return allIgnoredFiles.includes(targetFile)
-}
-
-function isAllowedFile(target) {
-  const targetPath = path.resolve(target)
-  const targetName = path.basename(targetPath)
-  const targetExt = path.extname(targetName)
-  var allowed = false
-  if (allAllowedFiletypes.includes(targetExt)) {
-    allAllowedFiles.forEach(allowedFile => {
-      if (allowed === true) {
-        return
-      }
-      if (targetName === allowedFile || targetName.startsWith(allowedFile)) {
-        allowed = true
-      }
-    })
-  }
-  return allowed
-}
-
-function isPermittedFile(target) {
-  const ignoredFile = isIgnoredFile(target)
-  const outsideRoot = isOutsideRoot(target)
-  const directory = isDirectory(target)
-  const allowed = isAllowedFile(target)
-
-  if (ignoredFile || outsideRoot || directory || !allowed) {
-    const messages = []
-    const relative = path.relative(root, target)
-    if (ignoredFile) {
-      messages.push(`${relative} is an ignored file.`)
-    }
-    if (outsideRoot) {
-      messages.push(`${relative} is outside the root directory.`)
-    }
-    if (directory) {
-      messages.push(`${relative} is a directory.`)
-    }
-    if (!allowed) {
-      messages.push(`${relative} is not an allowed file.`)
-    }
-    logMessage(messages)
-    return false
-  }
-
-  return true
-}
-
-
-// Check if the target is a directory.
-function isDirectory(target) {
-  try {
-    return fs.lstatSync(target).isDirectory()
-  }
-  catch (e) {
-    // throws an error if path doesnt exist
-    return false
-  }
-}
-
-// Restrict all readFile attempts to files within this directory.
-function isOutsideRoot(target) {
-  const relative = path.relative(root, path.resolve(target))
-  return Boolean(relative && relative.startsWith("..") && !path.isAbsolute(relative))
-}
-
-function getFirstExistingFile(filePaths, callback) {
-  var found = false
-
-  filePaths.forEach((filePath, i) => {
-    if (found === true) {
-      return
-    }
-    onFileExists(filePath, existingPath => {
-      if (found === true) {
-        return
-      }
-
-      found = true
-      callback(existingPath)
-    })
-
-    setTimeout(() => {
-      if ((i + 1) === filePaths.length) {
-        if (found === false) {
-          callback()
-        }
-      }
-    }, 1000)
-  })
-}
-
-function onFileExists(target, callback, errback) {
-  fs.exists(target, exists => {
-    if (exists) {
-      return callback(target)
-    }
-
-    //logMessage([`File doesn't exist: ${target}`])
-
-    if (errback) {
-      errback()
-    }
-  })
-}
-
-function readFile(target, callback) {
-  const permitted = isPermittedFile(target)
-  if (permitted === false) {
-    return callback(true, null)
-  }
-
-  onFileExists(target, () => {
-    fs.readFile(target, "utf8", (err, data) => {
-      if (err) {
-        return callback(true, null)
-      }
-
-      callback(null, data)
-    })
-  }, () => {
-    //logMessage([`File doesn't exist at ${target}`])
-    callback(true, null)
-  })
-}
-
-
-function readResponse(file, req, res) {
-  if (!hasSecretKey(req)) {
-    return res.sendStatus(400).end()
-  }
-
-  readFile(file, (err, data) => {
-    if (err) {
-      return res.sendStatus(404).end()
-    }
-
-    res.json(JSON.parse(data))
-  })
-}
-
-function renderResponse(req, res) {
-  var isRendered = false
-
-  if (!hasSecretKey(req)) {
-    return res.sendStatus(400).end()
-  }
-
-  bundlePaths.forEach((bp, i, arr) => {
-    if (isRendered === true) {
-      return
-    }
-
-    onFileExists(bp, bundlePathFound => {
-      if (isRendered === true) {
-        return
-      }
-
-      const rel = path.relative(root, bundlePathFound)
-
-      logMessage([`Successfully imported bundle. (${rel})`])
-
-      const renderBundle = importDefault(bundlePathFound)
-
-      renderBundle(req, context => {
-        isRendered = true
-        res.json(context)
-        logMessage([`Successfully rendered bundle. (${rel})`])
-      })
-    })
-
-    if (arr.length === (i + 1)) {
-      setTimeout(() => {
-        if (isRendered === false) {
-          logMessage([`Failed to find a bundle at ${bundlePath}`])
-          res.sendStatus(404).end()
-        }
-      }, 1000)
-    }
-  })
-}
-
-function listen(app) {
-  var afterMethod
-
-  app.listen(port, address, () => {
-    logMessage([`Server listening at http(s)://${address}:${port}`])
-    if (after) {
-      afterMethod = importDefault(after)
-    }
-
-    if (isFunction(afterMethod)) {
-      const relative = path.relative(root, after)
-
-      logMessage([`Running 'after' method. (${relative})`])
-      afterMethod(app, address, port)
-    }
-  })
-}
-
-function start(app) {
-  var beforeMethod
-  logMessage([`Allowed files: ${allAllowedFiles.join(", ")}`])
-  logMessage([`Allowed filetypes: ${allAllowedFiletypes.join(", ")}`])
-  logMessage([`Ignored files: ${allIgnoredFiles.join(", ")}`])
-
-  if (before) {
-    beforeMethod = importDefault(before)
-  }
-  if (isFunction(beforeMethod)) {
-    const relative = path.relative(root, before)
-
-    logMessage([`Running 'before' method. (${relative})`])
-    beforeMethod(app, listen)
-  }
-  else {
-    listen(app)
-  }
-}
+  statsFileName,
+  stateFileName,
+  allowedFiles,
+  allowedFiletypes,
+  ignoredFiles,
+  secretKey,
+  secretKeyHeaderName,
+)
 
 // Create the server
 const app = express()
 
-app.use(bodyParser.json({ limit: "2mb" }))
+app.use(bodyParser.json({ limit: "1mb" }))
 
 // Return the webpack stats for the agent/environment
-app.get(`${statsUrl}`, (req, res) => {
+app.get(`${statsUrl}`, (request, response) => {
+  setNoCacheHeaders(response)
   const relFilePath = `${statsPath}/${statsFileName}`
   const absFilePath = path.resolve(`./${relFilePath}`)
-  readResponse(absFilePath, req, res)
+  readResponse(
+    absFilePath,
+    request,
+    response,
+  )
 })
 
-
 // Returns the json data for the default state of a reducer.
-app.get(`${stateUrl}/:reducerName`, (req, res) => {
-  const { reducerName } = req.params
+app.get(`${stateUrl}/:reducerName`, (request, response) => {
+  setNoCacheHeaders(response)
+
+  const { reducerName } = request.params
 
   const stateFilePaths = [
     path.resolve(`./${statePath}/${reducerName}/${stateFileName}`),
@@ -447,16 +195,31 @@ app.get(`${stateUrl}/:reducerName`, (req, res) => {
 
   getFirstExistingFile(stateFilePaths, existingPath => {
     if (existingPath) {
-      return readResponse(existingPath, req, res)
+      return readResponse(
+        existingPath,
+        request,
+        response,
+      )
     }
 
-    res.sendStatus(404)
+    response.sendStatus(404)
   })
 })
 
 // Returns the rendered react component data.
-app.post(renderUrl, (req, res) => {
-  renderResponse(req, res)
+app.post(renderUrl, (request, response) => {
+  setNoCacheHeaders(response)
+  renderResponse(
+    paths,
+    request,
+    response,
+  )
 })
 
-start(app)
+app.listen(port, address, () => {
+  logMessage([`Server listening at http(s)://${address}:${port}`])
+  logMessage([`Allowed files: ${allowed.join(", ")}`])
+  logMessage([`Allowed filetypes: ${allowedTypes.join(", ")}`])
+  logMessage([`Ignored files: ${ignored.join(", ")}`])
+})
+
---
+++ utils.js
@@ -0,0 +1,301 @@
+const path = require("path")
+const fs = require("fs")
+
+function logMessage(lines) {
+  const first = `[react-ssr]: ${lines.shift()}`
+  const rest = lines.map(line => `  ${line}`)
+  const final = ([first].concat(rest)).join("\n")
+  console.log(final)
+}
+
+function isFunction(f) {
+  return Boolean(typeof f === "function")
+}
+
+function requireUncached(mod) {
+  delete require.cache[require.resolve(mod)]
+  return require(mod)
+}
+
+function requireModule(modulePath) {
+  try {
+    return requireUncached(modulePath).default
+  }
+  catch (e) {
+    return requireUncached(modulePath)
+  }
+}
+
+
+// Check if the target is a directory.
+function isDirectory(target) {
+  try {
+    return fs.lstatSync(target).isDirectory()
+  }
+  catch (e) {
+    // throws an error if path doesnt exist
+    return false
+  }
+}
+
+function getFirstExistingFile(filePaths, callback) {
+  var isFound = false
+
+  filePaths.forEach((filePath, i) => {
+    if (isFound === true) {
+      return
+    }
+    onFileExists(filePath, existingPath => {
+      if (isFound === true) {
+        return
+      }
+
+      isFound = true
+      callback(existingPath)
+    })
+    setTimeout(() => {
+      if ((i + 1) === filePaths.length) {
+        if (isFound === false) {
+          callback()
+        }
+      }
+    }, 1000)
+  })
+}
+
+function setNoCacheHeaders(res) {
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate")
+  res.header("Expires", "-1")
+  res.header("Pragma", "no-cache")
+}
+
+function onFileExists(target, callback, errback) {
+  fs.exists(target, exists => {
+    if (exists) {
+      return callback(target)
+    }
+
+    if (errback) {
+      errback()
+    }
+  })
+}
+
+function configure(
+  root,
+  bundlePath,
+  bundleName,
+  statsFileName,
+  stateFileName,
+  allowedFiles,
+  allowedFileTypes,
+  ignoredFiles,
+  secretKey,
+  secretKeyHeaderName,
+) {
+  // The paths used to find the bundle on the filesystem.
+  const bundlePathSuffix = path.join(bundlePath, bundleName)
+  const bundlePaths = [
+    bundleName,
+    path.resolve(root, `./${bundlePathSuffix}`),
+    path.resolve(root, `../../${bundlePathSuffix}`),
+    path.resolve(root, `../../../../${bundlePathSuffix}`),
+  ].filter(bp => {
+    if (isOutsideRoot(bp, root)) {
+      return false
+    }
+    return true
+  })
+
+  const opts = {
+    paths: bundlePaths,
+    allowed: [
+      statsFileName,
+      stateFileName,
+      ...allowedFiles,
+    ],
+    allowedTypes: [
+      ".json",
+      ...allowedFileTypes,
+    ],
+    ignored: [
+      ".env",
+      ".env.local",
+      ".env.development",
+      ".env.stage",
+      ".env.stage.local",
+      ".env.production",
+      ".env.production.local",
+      ...ignoredFiles,
+    ]
+  }
+
+  // Prevent reading of certain files always.
+  function isIgnoredFile(target) {
+    const targetPath = path.resolve(target)
+    const targetFile = path.basename(targetPath)
+    return opts.ignored.includes(targetFile)
+  }
+
+  function isAllowedFile(target) {
+    const targetPath = path.resolve(target)
+    const targetName = path.basename(targetPath)
+    const targetExt = path.extname(targetName)
+
+    var isAllowed = false
+    if (opts.allowedTypes.includes(targetExt)) {
+      opts.allowed.forEach(fileName => {
+        if (isAllowed === true) {
+          return
+        }
+        if (targetName === fileName || targetName.startsWith(fileName)) {
+          isAllowed = true
+        }
+      })
+    }
+    return isAllowed
+  }
+
+  function isPermittedFile(target) {
+    const ignoredFile = isIgnoredFile(target)
+    const outsideRoot = isOutsideRoot(target)
+    const directory = isDirectory(target)
+    const isAllowed = isAllowedFile(target)
+
+    if (ignoredFile || outsideRoot || directory || !isAllowed) {
+      const messages = []
+      const relative = path.relative(root, target)
+      if (ignoredFile) {
+        messages.push(`${relative} is an ignored file.`)
+      }
+      if (outsideRoot) {
+        messages.push(`${relative} is outside the __dirname directory.`)
+      }
+      if (directory) {
+        messages.push(`${relative} is a directory.`)
+      }
+      if (!isAllowed) {
+        messages.push(`${relative} is not an allowed file.`)
+      }
+      logMessage(messages)
+      return false
+    }
+
+    return true
+  }
+
+  // Restrict all readFile attempts to files within this directory.
+  function isOutsideRoot(target) {
+    const relative = path.relative(root, path.resolve(target))
+    return Boolean(relative && relative.startsWith("..") && !path.isAbsolute(relative))
+  }
+
+  // Returns true if the header exists and it matches the key specified.
+  function hasSecretKey(req) {
+    const headerValue = req.get(secretKeyHeaderName)
+    if (!secretKey || (headerValue === secretKey)) {
+      return true
+    }
+    return false
+  }
+
+  function readFile(target, callback) {
+    console.log(target)
+    const isPermitted = isPermittedFile(target)
+    if (isPermitted === false) {
+      return callback(true, null)
+    }
+
+    onFileExists(target, () => {
+      fs.readFile(target, "utf8", (err, data) => {
+        if (err) {
+          return callback(true, null)
+        }
+
+        callback(null, data)
+      })
+    }, () => {
+      callback(true, null)
+    })
+  }
+
+  function readResponse(
+    filePath,
+    request,
+    response,
+  ) {
+    if (!hasSecretKey(request)) {
+      return response.sendStatus(400).end()
+    }
+
+    readFile(filePath, (err, data) => {
+      if (err) {
+        return response.sendStatus(404).end()
+      }
+
+      response.json(JSON.parse(data))
+    })
+  }
+
+  function renderResponse(
+    bundlesSuspected,
+    request,
+    response,
+  ) {
+    if (!hasSecretKey(request)) {
+      return response.sendStatus(400).end()
+    }
+
+    var isRendered = false
+
+    bundlesSuspected.forEach((bp, i, arr) => {
+      if (isRendered === true) {
+        return
+      }
+
+      onFileExists(bp, bundlePathFound => {
+        if (isRendered === true) {
+          return
+        }
+
+        const rel = path.relative(root, bundlePathFound)
+        const renderBundle = requireModule(bundlePathFound)
+        logMessage([`Successfully imported bundle. (${rel})`])
+
+        renderBundle(request, context => {
+          response.json(context)
+          isRendered = true
+          logMessage([`Successfully rendered bundle. (${rel})`])
+        })
+      })
+
+      if (arr.length === (i + 1)) {
+        setTimeout(() => {
+          if (isRendered === false) {
+            response.sendStatus(404).end()
+          }
+        }, 1000)
+      }
+    })
+  }
+
+  return {
+    options: opts,
+    methods: {
+      renderResponse,
+      readResponse,
+    },
+  }
+}
+
+module.exports = {
+  configure,
+  onFileExists,
+  setNoCacheHeaders,
+  getFirstExistingFile,
+  isDirectory,
+  requireModule,
+  requireUncached,
+  isFunction,
+  logMessage,
+}
