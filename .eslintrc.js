ea43307c1c12598bc16ffc89a416198e3fe6d646
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-01-18 22:21:40 -0500

    chore: Adds eslint/prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,35 @@ const path = require("path")
 
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: "module",
   },
   settings: {
     "import/resolver": {
+      jest: {
+        jestConfigFile: "./jest.config.js",
+      },
       webpack: {
-        config: path.resolve("./webpack.config.dev.js")
-      }
-    }
+        config: path.resolve("./webpack.config.dev.js"),
+      },
+    },
   },
+  plugins: [
+    "node",
+    "package-json",
+    "import",
+    "simple-import-sort",
+    "jest",
+    "jest-formatting",
+  ],
   extends: [
-    "@alexseitsinger/eslint-config-base",
-    "@alexseitsinger/eslint-config-react",
-  ]
+    "@alexseitsinger/eslint-config/eslint",
+    "@alexseitsinger/eslint-config/node",
+    "@alexseitsinger/eslint-config/import",
+    "@alexseitsinger/eslint-config/package-json",
+    "@alexseitsinger/eslint-config/jest",
+    "@alexseitsinger/eslint-config/jest-formatting",
+    "@alexseitsinger/eslint-config/simple-import-sort",
+  ],
 }
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  parser: "babel",
+  semi: false,
+  useTabs: false,
+  proseWrap: "preserve",
+  arrowParens: "avoid",
+}
